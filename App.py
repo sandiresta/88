315499c13a1990fa1b20a32eb94014d164ccd4d4
sandiresta <atseridnas23@gmail.com>
2024-12-13 23:58:52 +0700

    Update App.py

--- App.py
+++ App.py
@@ -6,5 +6,5 @@ st.sidebar.title("Navigation")
 st.sidebar.write("Use the sidebar to navigate through the app.")
 
 st.write("# Welcome to the Image Processing App")
-st.image("C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\logo pu.png", width=200, caption="Our Beloved University")
+st.image( r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\logo pu.png", width=200, caption="Our Beloved University")
 st.write("This is the main page. Use the menu on the left to explore different sections.")
